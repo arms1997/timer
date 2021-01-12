e88ed5082536a48bad5b48b201d613e761368348
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-12 20:39:31 +0000

    updated timer function

--- timer.js
+++ timer.js
@@ -1,7 +1,11 @@
 const args = process.argv.slice(2).map((value) => Number(value)).sort((a, b) => a - b).filter((value) => Number.isInteger(value))
 
-for(let value of args){
-    setTimeout(()=> {
-      process.stdout.write('\x07')
-    }, value * 1000)
+const beep = function(value){
+  setTimeout(()=> {
+    process.stdout.write('\x07')
+  }, value * 1000)
+}
+
+for(let num of args){
+  beep(num)
 }
